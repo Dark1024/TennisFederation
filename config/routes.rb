345f7c5fc91cb928fb1b9a0fea8b897b9f8a7d8d
Joel Pineda <joel.pineda@unitec.edu>
2016-04-06 14:25:30 +0000

    Change Player to User Devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
 
   resources :matches
   resources :results
-  resources :players
-  devise_for :users, :controllers => { registrations: 'registrations' } do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
